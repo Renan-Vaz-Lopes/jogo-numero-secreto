2a0c440d1a13c3691cca840a28a9660296b6da19
Rvl <70848887+Renan-Vaz-Lopes@users.noreply.github.com>
2023-04-26 02:45:25 -0300

    poder usar apenas o prefixo "e"

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,7 +1,8 @@
-const elementoChute = document.getElementById('chute')
+const conteudoJs = document.getElementById('conteudoJs');
+const elementoChutes = new Array();
 
 window.SpeechRecognition = window.SpeechRecognition ||
-webkitSpeechRecognition;
+    webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
@@ -10,16 +11,62 @@ recognition.start();
 recognition.addEventListener('result', OnSpeak);
 
 function OnSpeak(e) {
-    chute = e.results[0][0].transcript
-    exibiChuteNaTela(chute);
-    verificaSeOChutePossuiUmValorValido(chute);
+    chute = e.results[0][0].transcript;
+    console.log("antes do replace:", chute);
+    chute = chute.replace('um', '1 ')
+    chute = chute.replace('dois', '2 ')
+    chute = chute.replace('três', '3 ')
+    chute = chute.replace('quatro', '4 ')
+    chute = chute.replace('cinco', '5 ')
+    chute = chute.replace('seis', '6 ')
+    chute = chute.replace('sete', '7 ')
+    chute = chute.replace('oito', '8 ')
+    chute = chute.replace('nove', '9 ')
+    chute = chute.replace('dez', '10 ')
+    chute = chute.replace('sem', '100 ')
+    console.log("depois do replace:", chute);
+    // console.log(chute);
+    // return false;
+    conteudoJs.innerHTML = ''
+
+    if (chute.toUpperCase() === "GAME OVER") {
+        document.body.innerHTML = `
+            <h2>GAME OVER!</h2>
+            <h3>O número secreto era ${numeroSecreto} </h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+        document.body.style.backgroundColor = "black";
+    }
+    
+
+    var chuteSemiFiltrado = chute.replace(/([^0-9])/g, '.');
+    var separandoString = chuteSemiFiltrado.split(".");
+    var chutes = new Array();
+    var j = 0;
+    for (let i = 0; i < separandoString.length; i++) {
+        if (separandoString[i] != "") {
+            chutes[j] = separandoString[i];
+            j++
+        }
+
+    }
+
+    exibiChuteNaTela(chutes);
 };
 
-function exibiChuteNaTela(chute) {
-    elementoChute.innerHTML = `
+function exibiChuteNaTela(chutes) {
+    for (i = 0; i < chutes.length; i++) {
+        conteudoJs.innerHTML += `
+        <br>
         <div>Você disse:</div>
-        <span class="box">${chute}</span>
-        <div 
+        <span class="box">${chutes[i]}</span>
     `
-}
-recognition.addEventListener('end', () => recognition.start())
+        verificaSeOChutePossuiUmValorValido(chutes[i]);
+    }
+};
+
+
+
+recognition.addEventListener('end', () => {
+    recognition.start()
+})
---
+++ app/validaChute.js
@@ -0,0 +1,47 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+
+        var numero = +chute;
+
+        if (chuteForInvalido(numero)) {
+            conteudoJs.innerHTML += `<div>valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+            return;
+        }
+
+        if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+            conteudoJs.innerHTML += `<div>valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+            return;
+        }
+
+        if (numero === numeroSecreto) {
+            document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto} </h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+            return;
+        } else if (numero > numeroSecreto) {
+            conteudoJs.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i> </div>
+        `
+        } else {
+            conteudoJs.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i> </div>
+        `
+        }
+
+    };
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+
+})
